ea5ece6620d8bb1735f1d0646107e217a5c4a101
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-03-05 02:59:21 +0600

    Category active button set

--- script.js
+++ script.js
@@ -12,12 +12,37 @@ const loadCategory = async () => {
 const displayCategories = (categories) => {
     const categoryContainer = document.getElementById('categories')
     categories.forEach(category => {
-        const newTag = document.createElement('li')
-        newTag.classList.add('hover:border-b-4', 'hover:border-b-red-700', 'text-xl', 'cursor-pointer')
-        newTag.innerText = `${category.title}`
-        categoryContainer.append(newTag)
+        const li = document.createElement('li')
+        // loadCategoryNews(li.id = category.id)
+        li.classList.add('hover:border-b-4', 'hover:border-b-red-700', 'text-xl', 'cursor-pointer')
+        li.innerText = `${category.title}`
+        categoryContainer.append(li)
+        // short version
+        // categoryContainer.innerHTML += `
+        // <li id= ${category.id} class="hover:border-b-4 hover:border-b-red-700 text-xl">${category.title} </li>
+        // `
     });
 
+    categoryContainer.addEventListener('click', (e) => {
+        const allLi = document.querySelectorAll('li')
+        // console.log(allLi);
+        allLi.forEach(li => {
+            li.classList.remove('border-b-4', 'border-b-red-700')
+        })
+
+        if (e.target.localName === 'li') {
+            e.target.classList.add('border-b-4', 'border-b-red-700')
+        }
+
+    })
 }
+
+// const loadCategoryNews = (id) => {
+//     console.log(id);
+
+// }
+
+
+
 
 loadCategory()
